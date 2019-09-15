356e733056458ec80e11463c278bf0db561e9b30
Kira <kawoods826@gmail.com>
2019-09-14 21:51:31 -0700

    added function to add articles to DOM via article.js file

--- Article/Article.js
+++ Article/Article.js
@@ -88,7 +88,8 @@ const data = [
   }
 ];
 
-/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
+/* Step 1: Create a function that creates a component. 
+You will want your component to look like the template below: 
   
   <div class="article">
     <h2>{title of the article}</h2>
@@ -101,14 +102,69 @@ const data = [
 
   Hint: You will need to use createElement more than once here!
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+  Your function should take either an object as it's one argument,
+  or 5 separate arguments mapping to each piece of the data object above.
 
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+  Step 2: Add an event listener to the expandButton span. 
+  This event listener should toggle the class 'article-open' on the 'article' div.
 
   Step 3: return the entire component.
 
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+  Step 4: Map over the data, creating a component for each oject 
+  and add each component to the DOM as children of the 'articles' div.
 
-  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
+  Step 5: Add a new article to the array. Make sure it is in the 
+  same format as the others. Refresh the page to see the new article.
 
 */
+
+function articleCreator(articleText){
+
+  const article = document.createElement('div');
+  article.classList.add('article')
+
+  const title = document.createElement('h2');
+  title.textContent = articleText.title;
+  article.appendChild(title);
+
+  const date = document.createElement('p');
+  date.classList.add('date');
+  date.textContent = articleText.date;
+  article.appendChild(date);
+
+  const paragraph1 = document.createElement('p');
+  paragraph1.textContent = articleText.firstParagraph; 
+  article.appendChild(paragraph1); 
+
+  const paragraph2 = document.createElement('p');
+  paragraph2.textContent = articleText.secondParagraph; 
+  article.appendChild(paragraph2); 
+
+  const paragraph3 = document.createElement('p');
+  paragraph3.textContent = articleText.thirdParagraph; 
+  article.appendChild(paragraph3); 
+
+  const button = document.createElement('span');
+  button.classList.add('expandButton');
+  button.textContent = "Expand Article";
+  button.addEventListener('click', (e) => {
+    article.classList.toggle('article-open');
+});
+  article.appendChild(button);
+
+  return article;
+}
+ 
+const article1 = articleCreator(data[0]);
+
+let articles = document.querySelector('.articles');
+articles.appendChild(article1);
+
+/* <div class="article">
+    <h2>{title of the article}</h2>
+    <p class="date">{date of the article}</p>
+
+    {three separate paragraph elements}
+
+    <span class='expandButton'></span>
+  </div> */
