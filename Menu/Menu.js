5ef30fd0284b5f4bfde5657c68c490c0716e2fa1
Kira <kawoods826@gmail.com>
2019-09-15 20:42:40 -0700

    completed MVP requirements for menu

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -21,12 +21,41 @@ let menuItems = [
 
   The function takes an array as its only argument.
 
+  */
+  const menu = document.createElement('div');
+  menu.classList.add('menu')
+  const header = document.querySelector('.header');
+  header.appendChild(menu);
+
+  const unorderedList = document.createElement('ul');
+  menu.appendChild(unorderedList);
+
+  function menuCreator(array){
+
+  const listItems = document.createElement('li');
+  listItems.textContent = array;
+  unorderedList.appendChild(listItems);
+
+  return menu;
+} 
+
+menuItems.forEach((arrayItem) => {
+  let newMenu = menuCreator(arrayItem);
+});
+
+const menuButton = document.querySelector('.menu-button');
+  menuButton.addEventListener('click', (e) => {
+  menu.classList.toggle('menu--open');
+});
+/*
   Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   Add those items to the <ul>
 
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+  Step 3: Using a DOM selector, select the menu button 
+  (the element with a class of 'menu-button') currently on the DOM.
 
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  Step 4: add a click event listener to the menu button. 
+  When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
 
   Step 5: return the menu component.
 
